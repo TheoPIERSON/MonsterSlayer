077751c2c7bc1908feb0419311a3903fd11fbce5
Theo Pierson <thheopierson@gmail.com>
2023-06-14 15:14:59 +0200

    build: create "you" and "monster" life level

--- main.js
+++ main.js
@@ -1,1 +1,23 @@
 "use strict";
+
+//create life bar
+const YourDiv = document.getElementById("you");
+const MonsterDiv = document.getElementById("monster");
+
+let yourLife = 100;
+let monsterAtk = 20;
+let monstersLife = 100;
+let yourAtk = 20;
+
+let yourActualLife = yourLife - monsterAtk;
+let monstersActualLife = monstersLife - yourAtk;
+
+let yourLifeBar = document.createElement("h2");
+yourLifeBar.textContent = yourActualLife;
+yourLifeBar.setAttribute("class", "player");
+YourDiv.appendChild(yourLifeBar);
+
+let monstersLifeBar = document.createElement("h2");
+monstersLifeBar.textContent = monstersActualLife;
+monstersLifeBar.setAttribute("class", "player");
+MonsterDiv.appendChild(monstersLifeBar);
